9f198c54006e181119086a4ff33b9c33c614e3a6
Syslabtech LLC <134613532+syslabtech@users.noreply.github.com>
2024-12-30 10:19:02 +0530

    remove db ping

--- main.go
+++ main.go
@@ -121,13 +121,13 @@ func init() {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
 	}
 
-	// Ping MongoDB
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		log.Fatalf("MongoDB Ping Error: %v", err)
-	}
+	// // Ping MongoDB
+	// ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	// defer cancel()
+	// err = client.Ping(ctx, nil)
+	// if err != nil {
+	// 	log.Fatalf("MongoDB Ping Error: %v", err)
+	// }
 
 	// Collections
 	keyCollection := client.Database("filemanager").Collection("keys")
